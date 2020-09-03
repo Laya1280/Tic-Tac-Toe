f3b78f4c8aa845a6fbd41a31cb9dff4cedc060e8
Laya1280 <layaml1280@gmail.com>
2020-09-03 20:42:32 +0530

    Second Stage

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -2,6 +2,12 @@ package tictactoe;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X\n O X O\n X X O");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("---------");
+        System.out.println("|" + input.substring(0,3).replace(""," ") + "|");
+        System.out.println("|" + input.substring(3,6).replace(""," ") + "|");
+        System.out.println("|" + input.substring(6,9).replace(""," ") + "|");
+        System.out.println("---------");
     }
 }
